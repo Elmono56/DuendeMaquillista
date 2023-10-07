0d3ac322c4d60347162da6c7ad2a737d65a30195
Javier Vasquez <javiervasquez212@gmail.com>
2023-10-07 00:34:27 -0600

    Feat: Navbar completed

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,7 +1,30 @@
 import React from "react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="bg-white text-black p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <div className="text-2xl font-bold">Enchanted Cosmetics</div>
+        <div className="space-x-4">
+          <button className="bg-pink-200 hover:bg-red-300 text-black py-2 px-4 rounded-lg">
+            Galería
+          </button>
+          <button className="bg-pink-200 hover:bg-red-300 text-black py-2 px-4 rounded-lg">
+            Productos
+          </button>
+          <button className="bg-pink-200 hover:bg-red-300 text-black py-2 px-4 rounded-lg">
+            Carrito
+          </button>
+          <button className="bg-pink-200 hover:bg-red-300 text-black py-2 px-4 rounded-lg">
+            Registrarse
+          </button>
+          <button className="bg-pink-200 hover:bg-red-300 text-black py-2 px-4 rounded-lg">
+            Iniciar sesión
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
