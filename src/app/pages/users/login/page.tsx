9edb82fa50f5f3dd489e2080a172263919c090a7
Javier Vasquez <javiervasquez212@gmail.com>
2023-10-07 00:34:45 -0600

    Test: Navbar to login

--- src/app/pages/users/login/page.tsx
+++ src/app/pages/users/login/page.tsx
@@ -1,17 +1,21 @@
 import React from "react";
 import BasicCard from "@/app/components/BasicCard";
+import Navbar from "@/app/components/Navbar";
 
 const Login = () => {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <BasicCard>
-        <input
-          type="text"
-          className="input-global"
-          placeholder="Escribe tu nombre de usuario"
-        />
-        <button className="boton-global">Iniciar</button>
-      </BasicCard>
+    <div>
+      <Navbar />
+      <div className="flex justify-center items-center h-screen">
+        <BasicCard>
+          <input
+            type="text"
+            className="input-global"
+            placeholder="Escribe tu nombre de usuario"
+          />
+          <button className="boton-global">Iniciar</button>
+        </BasicCard>
+      </div>
     </div>
   );
 };
