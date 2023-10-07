69a821a65078fe78d54956f23c60e073a1128a9e
Kevin <78829208+Kev1606@users.noreply.github.com>
2023-10-07 15:20:35 -0600

    Feat: EditProfile configured

--- src/app/pages/users/editProfile/page.tsx
+++ src/app/pages/users/editProfile/page.tsx
@@ -1,7 +1,47 @@
 import React from "react";
+import Navbar from "@/app/components/Navbar";
+import BasicCard from "@/app/components/BasicCard";
+import Footer from "@/app/components/Footer";
 
 const EditProfile = () => {
-  return <div>EditProfile</div>;
+  return (
+    <div>
+      <Navbar />
+      <div className="flex justify-center items-center h-screen">
+        <BasicCard>
+        <div className="text-2xl text-black font-bold lg:pb-[20px]">Editar Perfil</div>
+          <input
+            type="text"
+            className="input-global"
+            placeholder="Nombre"
+          />
+          <input
+            type="text"
+            className="input-global"
+            placeholder="Apellido"
+          />
+          <input
+            type="text"
+            className="input-global"
+            placeholder="Correo electrónico"
+          />
+          <input
+            type="text"
+            className="input-global"
+            placeholder="Contraseña actual"
+          />
+          <input
+            type="text"
+            className="input-global"
+            placeholder="Contraseña nueva"
+          />
+          <button className="boton-global">Guardar cambios</button>
+          <button className="boton-global">Volver</button>
+        </BasicCard>
+      </div>
+      <Footer/>
+    </div>
+  );
 };
 
 export default EditProfile;
