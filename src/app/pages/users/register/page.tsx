9d4f7cd9d487c83e2d61258a1eeaf1baf07ecf2a
Kevin <78829208+Kev1606@users.noreply.github.com>
2023-10-07 14:37:44 -0600

    Feat: Register completed

--- src/app/pages/users/register/page.tsx
+++ src/app/pages/users/register/page.tsx
@@ -1,7 +1,42 @@
 import React from "react";
+import Navbar from "@/app/components/Navbar";
+import BasicCard from "@/app/components/BasicCard";
+import Footer from "@/app/components/Footer";
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <div>
+      <Navbar />
+      <div className="flex justify-center items-center h-screen">
+        <BasicCard>
+        <div className="text-2xl text-black font-bold lg:pb-[20px]">Crear Cuenta</div>
+          <input
+            type="text"
+            className="input-global"
+            placeholder="Nombre"
+          />
+          <input
+            type="text"
+            className="input-global"
+            placeholder="Apellido"
+          />
+          <input
+            type="text"
+            className="input-global"
+            placeholder="Correo electrónico"
+          />
+          <input
+            type="password"
+            className="input-global"
+            placeholder="Contraseña"
+          />
+          <button className="boton-global">Iniciar</button>
+          <button className="boton-global">Volver</button>
+        </BasicCard>
+      </div>
+      <Footer/>
+    </div>
+  );
 };
 
 export default Register;
