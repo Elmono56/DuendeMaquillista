b9ac5432b2778b28f5d1993480472919f47da450
Javier Vasquez <javiervasquez212@gmail.com>
2023-10-06 21:47:53 -0600

    Feat: Defined tailwind css input/button on login page

--- src/app/pages/users/login/page.tsx
+++ src/app/pages/users/login/page.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <input
+        type="text"
+        className="bg-opacity-50 bg-gray-300 rounded-xl py-2 px-8 mb-4"
+        placeholder="Escribe tu nombre de usuario"
+      />
+      <button className="bg-pink-200 px-4 py-2 rounded-3xl hover:bg-pink-300">
+        Iniciar sesión
+      </button>
+    </div>
+  );
 };
 
 export default Login;
