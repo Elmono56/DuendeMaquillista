dcddbd1a4b591e7ff97767efbd23d19143aead9c
Javier Vasquez <javiervasquez212@gmail.com>
2023-10-06 21:29:47 -0600

    Feat: Button component finished

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
-const Button = () => {
-  return <div>Button</div>;
+interface ButtonProps {
+  text: string;
+}
+
+const Button: React.FC<ButtonProps> = ({ text }) => {
+  return <button className="bg-pink-200 px-4 py-2 rounded-3xl ">{text}</button>;
 };
 
 export default Button;
