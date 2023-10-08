079aefaeda6596a0dab3ab8ca2c502163cd23f91
Javier Vasquez <javiervasquez212@gmail.com>
2023-10-08 17:26:57 -0600

    feat: recoverPassword component finished

--- src/app/pages/users/recoverPassword/page.tsx
+++ src/app/pages/users/recoverPassword/page.tsx
@@ -1,7 +1,28 @@
 import React from "react";
+import BasicCard from "@/app/components/BasicCard";
+import Footer from "@/app/components/Footer";
+import Navbar from "@/app/components/Navbar";
 
 const RecoverPassword = () => {
-  return <div>RecoverPassword</div>;
+  return (
+    <div>
+      <Navbar />
+      <div className="flex justify-center items-center h-screen">
+        <BasicCard>
+          <div className="text-2xl text-black font-bold lg:pb-[20px]">
+            Recuperar contraseña
+          </div>
+          <input
+            type="text"
+            className="input-global"
+            placeholder="Correo electrónico"
+          />
+          <button className="boton-global">Recuperar</button>
+        </BasicCard>
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default RecoverPassword;
