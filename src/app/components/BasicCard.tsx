833746262dfb1d2a3291dbc8b3dcbefa06d17465
Javier Vasquez <javiervasquez212@gmail.com>
2023-10-08 17:30:47 -0600

    Fix: Set BasicCard component to only one size

--- src/app/components/BasicCard.tsx
+++ src/app/components/BasicCard.tsx
@@ -2,25 +2,16 @@ import React, { ReactNode } from "react";
 
 interface BasicCardProps {
   children: ReactNode;
-  size?: "small" | "medium" | "large"; // Define tamaños personalizados
 }
 
-const BasicCard = ({ children, size = "medium" }: BasicCardProps) => {
-  let cardSize = "w-2/4 h-96"; // Tamaño predeterminado
-
-  // Aplica clases de tamaño según la propiedad size
-  if (size === "small") {
-    cardSize = "w-2/4 h-64";
-  } else if (size === "large") {
-    cardSize = "w-2/4 h-120";
-  }
-
+const BasicCard = ({ children }: BasicCardProps) => {
   return (
-    <div className={`bg-white rounded-lg p-4 flex flex-col items-center justify-center ${cardSize}`}>
+    <div
+      className={`bg-white rounded-lg p-4 flex flex-col items-center justify-center w-2/4 h-120`}
+    >
       {children}
     </div>
   );
 };
-
 
 export default BasicCard;
