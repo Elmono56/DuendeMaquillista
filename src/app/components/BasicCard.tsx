c719b5221752c0182a80b64584731444c2205014
Javier Vasquez <javiervasquez212@gmail.com>
2023-10-08 17:33:39 -0600

    Fix: deleted extra in BasicCard h- style

--- src/app/components/BasicCard.tsx
+++ src/app/components/BasicCard.tsx
@@ -7,7 +7,7 @@ interface BasicCardProps {
 const BasicCard = ({ children }: BasicCardProps) => {
   return (
     <div
-      className={`bg-white rounded-lg p-4 flex flex-col items-center justify-center w-2/4 h-120`}
+      className={`bg-white rounded-lg p-4 flex flex-col items-center justify-center w-2/4`}
     >
       {children}
     </div>
