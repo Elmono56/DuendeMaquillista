2f721c54cb7ee84948b5b37c6d42da46722bf3e1
AxelCHCR <axelchaves.r@estudiantec.cr>
2023-10-11 00:08:32 -0600

    feat: post user working

--- src/app/api/connection.js
+++ src/app/api/connection.js
@@ -4,14 +4,14 @@ const dotenv = require("dotenv");
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 4000;
+const userRoutes = require("./routes/users");
 //middlewares
 app.use(express.json());
-
+app.use("/api", userRoutes);
 //routes
 app.get("/", (req, res) => {
   res.send("API for Duende Maquillista");
 });
-//console.log(process.env);
 //mongodb connection
 mongoose.connect("mongodb+srv://axelchavesr:tGfpOZBhreznmEQU@cluster0.jqlfuzl.mongodb.net/?retryWrites=true&w=majority")
 .then(() => console.log("MongoDB connected"))
--- src/app/api/routes/users.js
+++ src/app/api/routes/users.js
@@ -4,10 +4,12 @@ const userSchema = require("../models/user");
 const router = express.Router();
 
 //create user
-router.post("/user", (req, res)=>{
-    const user = userSchema(req.body);
-    user.save().then((data)=>res.json(data)).
-    catch((error)=>res.json({message: error}));
+router.post("/user", (req, res) => {
+  const user = userSchema(req.body);
+  user
+    .save()
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }));
 });
 
 module.exports = router;
