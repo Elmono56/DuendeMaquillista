5a75bc40ded51a9f757296354c48628ea5dbeddd
Kevin <78829208+Kev1606@users.noreply.github.com>
2023-10-11 00:47:08 -0600

    Feat: Cart component completed

---
+++ src/app/components/Cart.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+interface CardProps {
+  productName: string;
+  productPrice: string;
+  productImage: string;
+  quantity: number;
+}
+
+const Card = ({ productName, productPrice, productImage, quantity }: CardProps) => {
+
+  return (
+    <div className="bg-white rounded-lg p-4 w-904px h-94px p-4 bg-white">
+      <div className="flex flex-wrap space-x-4 justify-center items-center">
+        <button type="button" className="btn btn-danger rounded-full bg-red-200 w-[40px] h-[50px] p-4 text-black text-center">X</button>
+        <img className="object-cover border-black rounded-2xl" src={productImage} width={81} height={59} />
+        <h2 className="text-base text-black lg:w-[30%] p-4" >{productName}</h2>
+        <input className="bg-red-200 rounded-3xl h-[17px] w-[60px] p-4 text-black" type="number" min={1} value={quantity}/>
+        <p className="text-base text-black lg:w-[10%] p-4">{productPrice}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
--- src/app/pages/users/shoppingCart/page.tsx
+++ src/app/pages/users/shoppingCart/page.tsx
@@ -2,11 +2,22 @@ import React from "react";
 import Navbar from "@/app/components/Navbar";
 import BasicCard from "@/app/components/BasicCard";
 import Footer from "@/app/components/Footer";
+import Cart from "@/app/components/Cart";
 
 const ShoppingCart = () => {
   return (
     <>
       <Navbar />
+      <div className="flex justify-center items-center h-screen">
+        <BasicCard>
+          <Cart productName="Niacinamida" productPrice="$550" 
+                productImage="https://www.larocheposay-centroamerica.com/-/media/project/loreal/brand-sites/lrp/america/latam/products/niacinamide/pure-niacinamide-10-serum/3337875791885_1-1.jpg?cx=0&cy=0&cw=600&ch=600&hash=3C8F65B5BF784E6BA1165BFB424D6539D4628FC2" 
+                quantity={2} />
+          <Cart productName="Effaclar Duo" productPrice="$790" 
+                productImage="https://www.larocheposay-centroamerica.com/-/media/project/loreal/brand-sites/lrp/america/latam/products/effaclar/effaclar-duo-plus/larocheposayfacecareeffaclarduo40ml3337875598071front.png?cx=0.49&cy=0.63&cw=600&ch=600&hash=76733EBC9BE84DB9AB9209813CF22F46FD2CD4F8" 
+                quantity={1} />
+        </BasicCard>
+      </div>
       <Footer />
     </>
   );
