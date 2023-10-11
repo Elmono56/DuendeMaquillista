40824838e4aa2498cda5f143d0bc0948a32f4b37
Kevin <78829208+Kev1606@users.noreply.github.com>
2023-10-11 01:31:00 -0600

    Feat: ShoppingCart completed

--- src/app/pages/users/shoppingCart/page.tsx
+++ src/app/pages/users/shoppingCart/page.tsx
@@ -10,12 +10,25 @@ const ShoppingCart = () => {
       <Navbar />
       <div className="flex justify-center items-center h-screen">
         <BasicCard>
-          <Cart productName="Niacinamida" productPrice="$550" 
-                productImage="https://www.larocheposay-centroamerica.com/-/media/project/loreal/brand-sites/lrp/america/latam/products/niacinamide/pure-niacinamide-10-serum/3337875791885_1-1.jpg?cx=0&cy=0&cw=600&ch=600&hash=3C8F65B5BF784E6BA1165BFB424D6539D4628FC2" 
-                quantity={2} />
-          <Cart productName="Effaclar Duo" productPrice="$790" 
-                productImage="https://www.larocheposay-centroamerica.com/-/media/project/loreal/brand-sites/lrp/america/latam/products/effaclar/effaclar-duo-plus/larocheposayfacecareeffaclarduo40ml3337875598071front.png?cx=0.49&cy=0.63&cw=600&ch=600&hash=76733EBC9BE84DB9AB9209813CF22F46FD2CD4F8" 
-                quantity={1} />
+          <div className="text-2xl text-black font-bold lg:pb-[20px]">
+            Carrito de compras
+          </div>
+          <Cart productName="Niacinamida" productPrice="$550"
+            productImage="https://www.larocheposay-centroamerica.com/-/media/project/loreal/brand-sites/lrp/america/latam/products/niacinamide/pure-niacinamide-10-serum/3337875791885_1-1.jpg?cx=0&cy=0&cw=600&ch=600&hash=3C8F65B5BF784E6BA1165BFB424D6539D4628FC2"
+            quantity={2} />
+          <Cart productName="Effaclar Duo" productPrice="$790"
+            productImage="https://www.larocheposay-centroamerica.com/-/media/project/loreal/brand-sites/lrp/america/latam/products/effaclar/effaclar-duo-plus/larocheposayfacecareeffaclarduo40ml3337875598071front.png?cx=0.49&cy=0.63&cw=600&ch=600&hash=76733EBC9BE84DB9AB9209813CF22F46FD2CD4F8"
+            quantity={1} />
+          <div className="flex flex-wrap space-x-72" >
+            <div className="flex flex-grow-1 flex-col">            
+              <button className="bg-white text-black pb-5">+ Agregar producto</button>
+              <button className="bg-red-200 rounded-3xl text-black w-52">Ver mis pedidos</button>
+            </div>
+            <div className="flex flex-grow-1 flex-col">
+              <p className="bg-white text-black text-center pb-5">Subtotal: 128</p>
+              <button className="bg-red-200 rounded-3xl text-black w-52">Finalizar compra</button>
+            </div>
+          </div>
         </BasicCard>
       </div>
       <Footer />
