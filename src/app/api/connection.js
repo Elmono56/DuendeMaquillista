5fdfc117f98a36fd1866a9fabc2316a046d17926
AxelCHCR <axelchaves.r@estudiantec.cr>
2023-10-10 23:32:49 -0600

    merge

--- src/app/api/connection.js
+++ src/app/api/connection.js
@@ -3,18 +3,18 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
 const app = express();
-const port = process.env.PORT || 5000;
-
+const port = process.env.PORT || 4000;
 //middlewares
 app.use(express.json());
 
 //routes
-app.get("/", (req,res) => {
-    res.send("API for Duende Maquillista")
-})
-
+app.get("/", (req, res) => {
+  res.send("API for Duende Maquillista");
+});
+//console.log(process.env);
 //mongodb connection
-mongoose.connect(process.env.MONGODB_URI)
-.then(() => console.log("MongoDB connected"))
-.catch(err => console.log(err));
+mongoose
+  .connect("mongodb+srv://axelchavesr:tGfpOZBhreznmEQU@cluster0.jqlfuzl.mongodb.net/?retryWrites=true&w=majority")
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
 app.listen(port, () => console.log(`Server running on port ${port}`));
