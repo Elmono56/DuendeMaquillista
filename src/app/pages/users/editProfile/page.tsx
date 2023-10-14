b7a63ab84420f42310fc4feadd2f80e59f37a309
Javier Vasquez <javiervasquez212@gmail.com>
2023-10-13 23:44:41 -0600

    feat: Added usestates to EditProfile

--- src/app/pages/users/editProfile/page.tsx
+++ src/app/pages/users/editProfile/page.tsx
@@ -1,9 +1,25 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import Navbar from "@/app/components/Navbar";
 import BasicCard from "@/app/components/BasicCard";
 import Footer from "@/app/components/Footer";
 
 const EditProfile = () => {
+  const [nombre, setNombre] = useState("");
+  const [apellido, setApellido] = useState("");
+  const [email, setEmail] = useState("");
+  const [currentPassword, setCurrentPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+
+  const handleSaveChanges = () => {
+    console.log("Nombre:", nombre);
+    console.log("Apellido:", apellido);
+    console.log("Email:", email);
+    console.log("Contraseña actual:", currentPassword);
+    console.log("Contraseña nueva:", newPassword);
+  };
+
   return (
     <div>
       <Navbar />
@@ -12,24 +28,44 @@ const EditProfile = () => {
           <div className="text-2xl text-black font-bold lg:pb-[20px]">
             Editar Perfil
           </div>
-          <input type="text" className="input-global" placeholder="Nombre" />
-          <input type="text" className="input-global" placeholder="Apellido" />
+          <input
+            type="text"
+            className="input-global"
+            placeholder="Nombre"
+            value={nombre}
+            onChange={(e) => setNombre(e.target.value)}
+          />
+          <input
+            type="text"
+            className="input-global"
+            placeholder="Apellido"
+            value={apellido}
+            onChange={(e) => setApellido(e.target.value)}
+          />
           <input
             type="text"
             className="input-global"
             placeholder="Correo electrónico"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
           <input
-            type="text"
+            type="password"
             className="input-global"
             placeholder="Contraseña actual"
+            value={currentPassword}
+            onChange={(e) => setCurrentPassword(e.target.value)}
           />
           <input
-            type="text"
+            type="password"
             className="input-global"
             placeholder="Contraseña nueva"
+            value={newPassword}
+            onChange={(e) => setNewPassword(e.target.value)}
           />
-          <button className="boton-global">Guardar cambios</button>
+          <button className="boton-global" onClick={handleSaveChanges}>
+            Guardar cambios
+          </button>
         </BasicCard>
       </div>
       <Footer />
