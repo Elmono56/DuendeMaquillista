984a37d0852f948b150f465b6bb0a8c6cb212ed3
Kevin <78829208+Kev1606@users.noreply.github.com>
2023-10-17 13:47:20 -0600

    Feat: Gallery implemented

---
+++ src/app/pages/users/gallery/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "@/app/components/Navbar";
+import Footer from "@/app/components/Footer";
+
+const Gallery = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="flex justify-center items-center h-screen">
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default Gallery;
