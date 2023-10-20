6e3e888a5b05d40363e60cf67cd72e87d0683eed
Javier Vasquez <javiervasquez212@gmail.com>
2023-10-19 20:47:39 -0600

    feat: created Galería / Shop pages + routes for Navbar component

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
@@ -6,21 +7,31 @@ const Navbar = () => {
       <div className="container mx-auto flex justify-between items-center">
         <div className="text-2xl font-bold">Enchanted Cosmetics</div>
         <div className="space-x-4">
-          <button className="bg-pink-200 hover:bg-red-300 text-black py-2 px-4 rounded-lg">
-            Galería
-          </button>
-          <button className="bg-pink-200 hover:bg-red-300 text-black py-2 px-4 rounded-lg">
-            Productos
-          </button>
-          <button className="bg-pink-200 hover:bg-red-300 text-black py-2 px-4 rounded-lg">
-            Carrito
-          </button>
-          <button className="bg-pink-200 hover:bg-red-300 text-black py-2 px-4 rounded-lg">
-            Registrarse
-          </button>
-          <button className="bg-pink-200 hover:bg-red-300 text-black py-2 px-4 rounded-lg">
-            Iniciar sesión
-          </button>
+          <Link href="pages/admin/Catalog">
+            <button className="bg-pink-200 hover:bg-red-300 text-black py-2 px-4 rounded-lg">
+              Galería
+            </button>
+          </Link>
+          <Link href="/pages/admin/Shop">
+            <button className="bg-pink-200 hover:bg-red-300 text-black py-2 px-4 rounded-lg">
+              Productos
+            </button>
+          </Link>
+          <Link href="/pages/users/shoppingCart">
+            <button className="bg-pink-200 hover:bg-red-300 text-black py-2 px-4 rounded-lg">
+              Carrito
+            </button>
+          </Link>
+          <Link href="/pages/users/register">
+            <button className="bg-pink-200 hover:bg-red-300 text-black py-2 px-4 rounded-lg">
+              Registrarse
+            </button>
+          </Link>
+          <Link href="/">
+            <button className="bg-pink-200 hover:bg-red-300 text-black py-2 px-4 rounded-lg">
+              Iniciar sesión
+            </button>
+          </Link>
         </div>
       </div>
     </nav>
