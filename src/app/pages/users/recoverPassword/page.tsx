b21c83e0b6b2c3dd5e7eea3dc03eb5b11600edaa
Javier Vasquez <javiervasquez212@gmail.com>
2023-10-19 21:26:11 -0600

    feat: Added RecoverPassword routes + fix

--- src/app/pages/users/recoverPassword/page.tsx
+++ src/app/pages/users/recoverPassword/page.tsx
@@ -3,18 +3,11 @@ import React, { useState } from "react";
 import BasicCard from "@/app/components/BasicCard";
 import Footer from "@/app/components/Footer";
 import Navbar from "@/app/components/Navbar";
+import Link from "next/link";
 
 const RecoverPassword = () => {
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
-
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value);
-  };
-
-  const handleSubmit = () => {
-    setMessage("Solicitud de recuperación enviada con éxito.");
-  };
 
   return (
     <div>
@@ -29,11 +22,10 @@ const RecoverPassword = () => {
             className="input-global"
             placeholder="Correo electrónico"
             value={email}
-            onChange={handleEmailChange}
           />
-          <button className="boton-global" onClick={handleSubmit}>
-            Recuperar
-          </button>
+          <Link href="/">
+            <button className="boton-global">Recuperar</button>
+          </Link>
           {message && <p>{message}</p>}
         </BasicCard>
       </div>
