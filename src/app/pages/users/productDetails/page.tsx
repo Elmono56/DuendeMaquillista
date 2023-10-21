416fb50064a59e53712d61919986e71177af7e14
Javier Vasquez <javiervasquez212@gmail.com>
2023-10-21 16:39:08 -0600

    feat: improved ProductDetails

--- src/app/pages/users/productDetails/page.tsx
+++ src/app/pages/users/productDetails/page.tsx
@@ -1,53 +1,51 @@
-"use client";
-
 import Link from "next/link";
 import Navbar from "@/app/components/Navbar";
 
-const ProductDetails = () => {
-  return (
-    <div>
-      <Navbar />
-      <div className="flex justify-center items-center h-screen">
-        <div
-          className={`bg-white rounded-lg p-4 w-[45.625rem] h-[24.6875rem] justify-between`}
-        >
-          <h1 className="text-center text-3xl py-3">Detalles del producto</h1>
-          <div className=" flex flex-row items-center justify-center justify-between">
-            <div className="flex-1 flex justify-center items-center">
-              <div className="rounded-md border border-black w-72 h-72"></div>
-            </div>
-            <div className="flex-1 flex items-center justify-center">
-              <div className="flex flex-col items-center space-y-8">
-                <label>Categoría:</label>
-                <label>Subcategoría:</label>
-                <label>Descripción:</label>
-                <label>Disponibles:</label>
-                <label>Precio:</label>
-              </div>
-            </div>
+const ProductDetails = () => (
+  <div className="bg-pink-lighter min-h-screen">
+    <Navbar />
+    <main className="flex justify-center items-center h-screen p-5">
+      <div className="bg-white rounded-lg shadow-lg p-6 max-w-2xl transition-all transform hover:scale-105">
+        <h1 className="text-center text-2xl py-2 mb-3 border-b-2">
+          Detalles del producto
+        </h1>
 
-            <div className="flex-1 flex items-center justify-center">
-              <div className="flex flex-col items-center space-y-8">
-                <div className="flex flex-row items-center">
-                  <label className="text-sm">Cantidad:</label>
-                </div>
-                <input
-                  type="number"
-                  id="cantidad"
-                  className="w-20 h-8 p-2 text-sm border rounded"
-                  value="2"
-                />
-                <label>Total: </label>
-                <Link href="/pages/users/shoppingCart">
-                  <button className="boton-global">Agregar al carrito</button>
-                </Link>
-              </div>
+        <div className="flex flex-row items-start justify-between mt-5 space-x-6">
+          <div className="flex-1 flex justify-center items-center">
+            <div className="rounded-md border border-gray-300 w-80 h-80 transition duration-300 hover:border-blue-500"></div>
+          </div>
+
+          <div className="flex-1 flex flex-col items-start space-y-3">
+            <label className="text-gray-700">Categoría:</label>
+            <label className="text-gray-700">Subcategoría:</label>
+            <label className="text-gray-700">Descripción:</label>
+            <label className="text-gray-700">Disponibles:</label>
+            <label className="text-gray-700 font-medium">Precio:</label>
+            <div className="flex items-center space-x-2 mt-3">
+              <label className="text-sm font-medium text-gray-700">
+                Cantidad:
+              </label>
+              <input
+                type="number"
+                id="cantidad"
+                className="w-20 h-8 p-1 text-sm border rounded bg-gray-50 focus:border-blue-500"
+                value="2"
+              />
             </div>
           </div>
         </div>
+
+        <div className="mt-5">
+          <label className="text-lg font-medium text-gray-700 block mb-3">
+            Total:
+          </label>
+          <Link href="/pages/users/shoppingCart">
+            <button className="boton-global">Agregar al carrito</button>
+          </Link>
+        </div>
       </div>
-    </div>
-  );
-};
+    </main>
+  </div>
+);
 
 export default ProductDetails;
