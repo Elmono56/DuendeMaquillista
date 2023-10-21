c6f88b6d8a996ced4fafa3d97632bd6e6096f429
Javier Vasquez <javiervasquez212@gmail.com>
2023-10-21 16:38:04 -0600

    feat: improved navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -4,69 +4,64 @@ import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <nav>
-      <div className="flex flex-row justify-between bg-pink-lighter py-4 ml-4">
-        <div>
+    <nav className="bg-pink-lighter">
+      <div className="container mx-auto flex justify-between items-center py-4 px-8">
+        <div className="flex items-center">
           <Image
             src="/img/makeup-woman-svgrepo-com.svg"
             width={60}
             height={60}
             alt="Makeup Woman"
           />
+          <span className="ml-4 font-semibold text-2xl">
+            Enchanted Cosmetics
+          </span>
         </div>
-        <div className="font-semibold text-2xl flex items-center">
-          Enchanted Cosmetics
-        </div>
-        <div>
-          <div className="flex flex-row">
-            <Image
-              src="/img/user-svgrepo-com.svg"
-              width={24}
-              height={24}
-              alt="User"
-            />
-            <button className="mr-4 ">Sing Up</button>
-            <button className="mr-4 ">Sing In</button>
-          </div>
-          <div className="flex py-2">
-            <button className="mr-2">CARRITO</button>
-            <Image
-              src="/img/cart-shopping-svgrepo-com.svg"
-              width={24}
-              height={24}
-              alt="Cart"
-            />
-          </div>
+        <div className="flex items-center space-x-4">
+          <Image
+            src="/img/user-svgrepo-com.svg"
+            width={24}
+            height={24}
+            alt="User"
+          />
+          <button className="mr-4 hover:underline">Sign Up</button>
+          <button className="mr-4 hover:underline">Sign In</button>
+          <button className="mr-4 hover:underline">CARRITO</button>
+          <Image
+            src="/img/cart-shopping-svgrepo-com.svg"
+            width={24}
+            height={24}
+            alt="Cart"
+          />
         </div>
       </div>
-      <div className="flex w-full justify-between bg-red-200">
-        <Link
-          href="/pages/admin/Catalog"
-          className="flex-grow border border-black text-center py-1"
-        >
-          <button>Galería</button>
-        </Link>
-
-        <Link
-          href="/pages/admin/Shop"
-          className="flex-grow border border-black text-center py-1"
-        >
-          <button>Tienda</button>
-        </Link>
-
-        <Link
-          href="/"
-          className="flex-grow border border-black text-center py-1"
-        >
-          <button>Agenda</button>
-        </Link>
-
-        <Link
-          href="/"
-          className="flex-grow border border-black text-center py-1"
-        >
-          <button>Sing out</button>
-        </Link>
+      <div className="border-t border-pink-400 bg-red-200">
+        <div className="container mx-auto flex justify-between items-center py-2">
+          <Link
+            href="/pages/admin/Catalog"
+            className="flex-grow border border-black text-center py-1 hover:bg-hover-pink transition duration-300"
+          >
+            <button>Galería</button>
+          </Link>
+          <Link
+            href="/pages/admin/Shop"
+            className="flex-grow border border-black text-center py-1 hover:bg-hover-pink transition duration-300"
+          >
+            <button>Tienda</button>
+          </Link>
+          <Link
+            href="/"
+            className="flex-grow border border-black text-center py-1 hover:bg-hover-pink transition duration-300"
+          >
+            <button>Agenda</button>
+          </Link>
+          <Link
+            href="/"
+            className="flex-grow border border-black text-center py-1 hover:bg-hover-pink transition duration-300"
+          >
+            <button>Sign Out</button>
+          </Link>
+        </div>
       </div>
     </nav>
   );
