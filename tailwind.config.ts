00fd24cd9ac54e585e4c000feafcc70ef35872f1
Javier Vasquez <javiervasquez212@gmail.com>
2023-10-21 16:37:54 -0600

    feat: added new color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,12 +11,14 @@ const config: Config = {
       colors: {
         'pink-lighter': '#ffef6',
         'gray-input': '#d1d5db',
+        'hover-pink': '#ed8f8f',
       },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
       fontFamily: {
+        'sans': ['Playfair Display', 'sans-serif'], // Aquí la estamos estableciendo como predeterminada
         'playfair': ['Playfair Display'],
       },
     },
