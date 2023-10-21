e48e80be49b659ee578017385a9c455b317bd04e
Javier Vasquez <javiervasquez212@gmail.com>
2023-10-21 15:34:26 -0600

    fix: deleted gradients

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -3,18 +3,11 @@ import React from "react";
 const Footer = () => {
   return (
     <>
-      <footer className="relative bg-white pt-20 pb-2 lg:pt-[30px]">
+      <footer className="relative bg-pink-lighter pt-20 pb-2 lg:pt-[30px]">
         <div className="container">
           <div className="text-black flex flex-wrap -mx-4">
             <div className="w-full px-4 lg:w-[55%] lg:pl-[55px]">
               <div className="w-full mb-10">
-                {/* <a href="/#" className="mb-6 inline-block max-w-[160px]">
-                  <img
-                    src=""
-                    alt="logo"
-                    className="max-w-full"
-                  />
-                </a> */}
                 <div className="text-2xl font-bold lg:pb-[40px]">
                   Enchanted Cosmetics
                 </div>
@@ -135,19 +128,6 @@ const Footer = () => {
                 d="M-64 140.5C-64 62.904 -1.096 1.90666e-05 76.5 1.22829e-05C154.096 5.49924e-06 217 62.904 217 140.5C217 218.096 154.096 281 76.5 281C-1.09598 281 -64 218.096 -64 140.5Z"
                 fill="url(#paint0_linear_1179_5)"
               />
-              <defs>
-                <linearGradient
-                  id="paint0_linear_1179_5"
-                  x1="76.5"
-                  y1={281}
-                  x2="76.5"
-                  y2="1.22829e-05"
-                  gradientUnits="userSpaceOnUse"
-                >
-                  <stop stopColor="#3056D3" stopOpacity="0.08" />
-                  <stop offset={1} stopColor="#C4C4C4" stopOpacity={0} />
-                </linearGradient>
-              </defs>
             </svg>
           </span>
           <span className="absolute top-10 right-10 z-[-1]">
@@ -162,19 +142,6 @@ const Footer = () => {
                 d="M37.5 -1.63918e-06C58.2107 -2.54447e-06 75 16.7893 75 37.5C75 58.2107 58.2107 75 37.5 75C16.7893 75 -7.33885e-07 58.2107 -1.63918e-06 37.5C-2.54447e-06 16.7893 16.7893 -7.33885e-07 37.5 -1.63918e-06Z"
                 fill="url(#paint0_linear_1179_4)"
               />
-              <defs>
-                <linearGradient
-                  id="paint0_linear_1179_4"
-                  x1="-1.63917e-06"
-                  y1="37.5"
-                  x2={75}
-                  y2="37.5"
-                  gradientUnits="userSpaceOnUse"
-                >
-                  <stop stopColor="#13C296" stopOpacity="0.31" />
-                  <stop offset={1} stopColor="#C4C4C4" stopOpacity={0} />
-                </linearGradient>
-              </defs>
             </svg>
           </span>
         </div>
