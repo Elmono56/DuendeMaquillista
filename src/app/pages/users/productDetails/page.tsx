6becf3871fbf7d5cebe97b9d54370aba876f76a3
Javier Vasquez <javiervasquez212@gmail.com>
2023-10-21 12:39:23 -0600

    fix: improved ProductDetails page

--- src/app/pages/users/productDetails/page.tsx
+++ src/app/pages/users/productDetails/page.tsx
@@ -8,33 +8,40 @@ const ProductDetails = () => {
     <div>
       <Navbar />
       <div className="flex justify-center items-center h-screen">
-        <div className="bg-white rounded-lg h-[18rem] w-[40.5rem] flex flex-row">
-          <div className="flex-1 flex justify-center items-center">
-            <div className="rounded-xl border border-black w-48 h-48"></div>
-          </div>
-          <div className="flex-1 flex items-center justify-center">
-            <div className="flex flex-col items-center space-y-8">
-              <label>Descripción:</label>
-              <label>Disponibles:</label>
-              <label>Precio:</label>
+        <div
+          className={`bg-white rounded-lg p-4 w-[45.625rem] h-[24.6875rem] justify-between`}
+        >
+          <h1 className="text-center text-3xl py-3">Detalles del producto</h1>
+          <div className=" flex flex-row items-center justify-center justify-between">
+            <div className="flex-1 flex justify-center items-center">
+              <div className="rounded-md border border-black w-72 h-72"></div>
             </div>
-          </div>
+            <div className="flex-1 flex items-center justify-center">
+              <div className="flex flex-col items-center space-y-8">
+                <label>Categoría:</label>
+                <label>Subcategoría:</label>
+                <label>Descripción:</label>
+                <label>Disponibles:</label>
+                <label>Precio:</label>
+              </div>
+            </div>
 
-          <div className="flex-1 flex items-center justify-center">
-            <div className="flex flex-col items-center space-y-8">
-              <div className="flex flex-row items-center">
-                <label className="text-sm">Cantidad:</label>
+            <div className="flex-1 flex items-center justify-center">
+              <div className="flex flex-col items-center space-y-8">
+                <div className="flex flex-row items-center">
+                  <label className="text-sm">Cantidad:</label>
+                </div>
                 <input
                   type="number"
                   id="cantidad"
                   className="w-20 h-8 p-2 text-sm border rounded"
                   value="2"
                 />
+                <label>Total: </label>
+                <Link href="/pages/users/shoppingCart">
+                  <button className="boton-global">Agregar al carrito</button>
+                </Link>
               </div>
-              <label>Total: </label>
-              <Link href="/pages/users/shoppingCart">
-                <button className="boton-global">Agregar al carrito</button>
-              </Link>
             </div>
           </div>
         </div>
