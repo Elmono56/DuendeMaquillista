5dfeb5ea548087e96ec43d8e9b6c4b9626fc3073
Kevin <78829208+Kev1606@users.noreply.github.com>
2023-10-22 01:07:02 -0600

    Fix: certains div fixed

--- src/app/pages/users/endPurchase/page.tsx
+++ src/app/pages/users/endPurchase/page.tsx
@@ -9,20 +9,28 @@ const EndPurchase = () => {
     <div>
       <Navbar />
       <div className="flex justify-center items-center h-screen">
-        <div className="w-2/4 bg-white rounded-lg h-549 w-384">
+        <div className="h-3/5 bg-white rounded-lg overflow-y-scroll">
           <div className="flex flex-row">
-            <Cart
-              productName="Niacinamida"
-              productPrice="$550"
-              productImage="https://www.larocheposay-centroamerica.com/-/media/project/loreal/brand-sites/lrp/america/latam/products/niacinamide/pure-niacinamide-10-serum/3337875791885_1-1.jpg?cx=0&cy=0&cw=600&ch=600&hash=3C8F65B5BF784E6BA1165BFB424D6539D4628FC2"
-              quantity={2}
-              editable={false}
-            />
-            <div className="pt-5">
+            <div className="overflow-y-scroll">
+              <Cart
+                productName="Niacinamida"
+                productPrice="$550"
+                productImage="https://www.larocheposay-centroamerica.com/-/media/project/loreal/brand-sites/lrp/america/latam/products/niacinamide/pure-niacinamide-10-serum/3337875791885_1-1.jpg?cx=0&cy=0&cw=600&ch=600&hash=3C8F65B5BF784E6BA1165BFB424D6539D4628FC2"
+                quantity={2}
+                editable={false}
+              />
+            </div>
+            <div className="flex flex-col pt-5">
               <div className="w-72 h-64 rounded-xl border border-black m-4"></div>
               <button className="boton-global">
                 Añadir comprobante de sinpe
               </button>
+              <p className="flex justify-center mt-4 p-4 text-black self-center">
+                Envio:
+              </p>
+              <p className="flex justify-center mt-4 p-4 text-black self-center">
+                Subtotal:
+              </p>
             </div>
           </div>
           <div className="flex flex-wrap items-center">
@@ -30,13 +38,15 @@ const EndPurchase = () => {
               <p className="flex justify-left items-center m-4 p-4 text-black">
                 Total:
               </p>
-              <p className="flex justify-left items-center m-4 p-4 text-black">
-                Nombre:
-              </p>
-              <p className="flex justify-left items-center m-4 p-4 text-black">
-                Apellido:
-              </p>
-              <div className="flex flex-col m-4">
+              <div className="flex flex-row space-x-14">
+                <p className="flex justify-left items-center m-4 p-4 text-black">
+                  Nombre:
+                </p>
+                <p className="flex justify-left items-center m-4 p-4 text-black">
+                  Apellidos:
+                </p>
+              </div>
+              <div className="flex flex-col p-4">
                 <input
                   type="text"
                   placeholder="Dirección de envío"
@@ -48,11 +58,6 @@ const EndPurchase = () => {
                   className="input-global"
                 />
               </div>
-            </div>
-            <div className="flex flex-col self-start ml-16">
-              <p className="flex justify-left mt-4 p-4 text-black self-start">
-                Subtotal:
-              </p>
             </div>
           </div>
           <div className="flex justify-center items-center m-4">
