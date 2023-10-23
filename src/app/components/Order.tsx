8cdfb305843204ba6f9c8deecd768d6b83a1cc66
Kevin <78829208+Kev1606@users.noreply.github.com>
2023-10-23 12:58:06 -0600

    Feat: API implemented without an action logic

--- src/app/components/Order.tsx
+++ src/app/components/Order.tsx
@@ -2,17 +2,26 @@ import React from 'react';
 
 interface OrderProps {
   idOrder: string;
+  onAction: (actionType: string, idOrder: string) => void;
 }
 
-const Order = ({ idOrder }: OrderProps) => {
+const Order = ({ idOrder, onAction }: OrderProps) => {
   return (
     <div className="bg-white rounded-lg p-4 w-920px h-94px p-4 bg-white">
     <div className="flex flex-wrap space-x-4 items-center">
-      <button type="button" className="btn btn-danger rounded-full bg-red-200 w-[40px] h-[50px] p-4 text-black text-center">X</button>
-      <h2 className="text-base text-black lg:w-[24%] p-4" >ID: {idOrder}</h2>
-      <button className='boton-global'>Detalles</button>
-      <button className='boton-global'>Confirmar</button>
-      <button className='boton-global'>Rechazar</button>
+      <button type="button" className="btn btn-danger rounded-full bg-red-200 w-[30px] h-[30px] text-black text-center"
+        onClick={() => onAction('decline', idOrder)}>
+        X
+      </button>
+      <h2 className="text-base text-black p-4" >ID: {idOrder}</h2>
+      <div className='flex w-1/3 space-x-2'>
+        <button className='boton-global'
+          onClick={() => onAction('details', idOrder)}>Detalles</button>
+        <button className='boton-global'
+          onClick={() => onAction('confirm', idOrder)}>Confirmar</button>
+        <button className='boton-global'
+          onClick={() => onAction('decline', idOrder)}>Rechazar</button>
+      </div>
     </div>
   </div>
   );
--- src/app/pages/admin/orders/page.tsx
+++ src/app/pages/admin/orders/page.tsx
@@ -1,11 +1,52 @@
 "use client";
 
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Order from "@/app/components/Order";
 import Navbar from "@/app/components/Navbar";
 import BasicCard from "@/app/components/BasicCard";
+import axios from "axios";
 
 const Orders = () => {
+  const [orderNum, setOrderNum] = useState("");
+  const [orders, setOrders] = useState([]);
+  useEffect(() => {
+    async function getData() {
+      try {
+        const res = await axios.get("http://localhost:4000/api/getOrders");
+        console.log(res.data);
+        const extractedIds = res.data.map((order: { _id: string }) => {
+          const truncatedId = order._id.slice(0, 24); // Recorta a 24 dígitos (todos)
+          return { _id: truncatedId };
+        });
+        console.log(extractedIds);
+        setOrders(extractedIds);
+        console.log(orders); 
+      } catch (error: any) {
+        console.log("PASO ALGO ", error);
+      }
+    }
+    getData();
+  }, []);
+
+  const handleAction = (actionType: string, idOrder: string) => {
+    switch (actionType) {
+      case 'details':
+        // Aquí se redirige a la página de detalles de la orden
+        console.log('Detalles de la orden', idOrder);
+        break;
+      case 'confirm':
+        // Aquí se confirma la orden
+        console.log('Confirmar orden', idOrder);
+        break;
+      case 'decline':
+        // Aquí se rechaza la orden
+        console.log('Rechazar orden', idOrder);
+        break;
+      default:
+        break;
+    }
+  }
+
   return (
     <>
       <Navbar />
@@ -15,12 +56,9 @@ const Orders = () => {
             Pedidos
           </div>
           <div className="scrolling-auto">
-            <Order idOrder="11928663" />
-            <Order idOrder="11928664" />
-            <Order idOrder="11928665" />
-            <Order idOrder="11928666" />
-            <Order idOrder="11928667" />
-            <Order idOrder="11928668" />
+            {orders.map((order: { _id: string }) => (
+              <Order key={order._id} idOrder={order._id} onAction={handleAction}/>
+            ))}
           </div>
         </BasicCard>
       </div>
