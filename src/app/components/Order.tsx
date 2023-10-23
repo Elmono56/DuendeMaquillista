cbf0ec59ab69d45191e5a79902a297dafc4fcfd5
Javier Vasquez <javiervasquez212@gmail.com>
2023-10-23 17:51:18 -0600

    feat: added route to Order admin component

--- src/app/components/Order.tsx
+++ src/app/components/Order.tsx
@@ -1,4 +1,7 @@
-import React from 'react';
+"use client";
+
+import React from "react";
+import Link from "next/link";
 
 interface OrderProps {
   idOrder: string;
@@ -8,23 +11,40 @@ interface OrderProps {
 const Order = ({ idOrder, onAction }: OrderProps) => {
   return (
     <div className="bg-white rounded-lg p-4 w-920px h-94px p-4 bg-white">
-    <div className="flex flex-wrap space-x-4 items-center">
-      <button type="button" className="btn btn-danger rounded-full bg-red-200 w-[30px] h-[30px] text-black text-center"
-        onClick={() => onAction('decline', idOrder)}>
-        X
-      </button>
-      <h2 className="text-base text-black p-4" >ID: {idOrder}</h2>
-      <div className='flex w-1/3 space-x-2'>
-        <button className='boton-global'
-          onClick={() => onAction('details', idOrder)}>Detalles</button>
-        <button className='boton-global'
-          onClick={() => onAction('confirm', idOrder)}>Confirmar</button>
-        <button className='boton-global'
-          onClick={() => onAction('decline', idOrder)}>Rechazar</button>
+      <div className="flex flex-wrap space-x-4 items-center">
+        <button
+          type="button"
+          className="btn btn-danger rounded-full bg-red-200 w-[30px] h-[30px] text-black text-center"
+          onClick={() => onAction("decline", idOrder)}
+        >
+          X
+        </button>
+        <h2 className="text-base text-black p-4">ID: {idOrder}</h2>
+        <div className="flex w-1/3 space-x-2">
+          <Link href="/pages/users/orderDetails">
+            <button
+              className="boton-global"
+              onClick={() => onAction("details", idOrder)}
+            >
+              Detalles
+            </button>
+          </Link>
+          <button
+            className="boton-global"
+            onClick={() => onAction("confirm", idOrder)}
+          >
+            Confirmar
+          </button>
+          <button
+            className="boton-global"
+            onClick={() => onAction("decline", idOrder)}
+          >
+            Rechazar
+          </button>
+        </div>
       </div>
     </div>
-  </div>
   );
-}
+};
 
 export default Order;
