a297110dc1981da6c3237e4e20bd633ff30dd1e6
Kevin <78829208+Kev1606@users.noreply.github.com>
2023-10-26 19:36:34 -0600

    Feat: logic implemented for searches by name

--- src/app/pages/admin/Shop/page.tsx
+++ src/app/pages/admin/Shop/page.tsx
@@ -75,197 +75,225 @@ const Shop = () => {
   // Estado para el menú desplegable
   const [dropdownVisible, setDropdownVisible] = useState(null);
 
-  const toggleDropdown = (idx) => {
-    if (dropdownVisible === idx) {
-      setDropdownVisible(null);
-    } else {
-      setDropdownVisible(idx);
+  // Estado para el buscador
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const handleSearch = () => {
+    // Realiza la búsqueda utilizando la consulta de búsqueda (searchQuery)
+    // Puedes realizar la búsqueda en el estado gallery o hacer una nueva solicitud al servidor, dependiendo de tus necesidades.
+
+    const results = gallery.filter((item) =>
+      item.title.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+    setSearchResults(results);
+  };
+
+  // useEffect(() => {
+  //     const results = gallery.filter((item) =>
+  //       item.title.toLowerCase().includes(searchQuery.toLowerCase())
+  //     );
+  //     setSearchResults(results);
+  //   }
+  //     , [searchQuery]);
+
+
+    // Renderiza los items 
+    const [searchResults, setSearchResults] = useState<Array<{ id: string, imgSrc: string, title: string, price: string }>>([]);
+    const renderItems = searchResults.length > 0 ? searchResults : gallery;
+
+    const toggleDropdown = (idx) => {
+      if (dropdownVisible === idx) {
+        setDropdownVisible(null);
+      } else {
+        setDropdownVisible(idx);
+      }
+    };
+
+    useEffect(() => {
+      // Aqui se hace la peticion para obtener los productos
+      // y se actualiza el estado de la tienda
+      async function getProducts() {
+        const res = await axios.get("http://localhost:4000/api/getProducts");
+        // Actualiza el estado gallery con la matriz de productos
+        setGallery(res.data.map((product: { _id: string, imageURL: string, name: string, price: string }) => ({
+          id: product._id,
+          imgSrc: product.imageURL,
+          title: product.name,
+          price: "$" + product.price,
+        })));
+      } getProducts();
     }
-  };
+      , []);
 
-  useEffect(() => {
-    // Aqui se hace la peticion para obtener los productos
-    // y se actualiza el estado de la tienda
-    async function getProducts() {
-      const res = await axios.get("http://localhost:4000/api/getProducts");
-      // Actualiza el estado gallery con la matriz de productos
-      setGallery(res.data.map((product: { _id: string, imageURL: string, name: string, price: string }) => ({
-        id: product._id,
-        imgSrc: product.imageURL,
-        title: product.name,
-        price: "$" + product.price,
-      })));
-    } getProducts();
-  }
-    , []);
+    const handleDeleteProduct = async (id: string) => {
+      console.log("ID: ", id);
+      const confirm = window.confirm("¿Estás seguro?");
+      if (confirm) {
+        // Aqui se hace la peticion para eliminar el producto
+        // const res = await axios.put("http://localhost:4000/api/setProductVisible", { id, visible: false });
+      }
+    };
 
-  const handleDeleteProduct = async (id: string) => {
-    console.log("ID: ", id);
-    const confirm = window.confirm("¿Estás seguro?");
-    if (confirm) {
-      // Aqui se hace la peticion para eliminar el producto
-      // const res = await axios.put("http://localhost:4000/api/setProductVisible", { id, visible: false });
+    const handleEditProduct = (id: string) => {
+      // Navegar a la página de editProduct y pasar el ID del producto como un parámetro en la URL
+      localStorage.setItem('productID', id);
+      router.push("/pages/admin/editProduct");
     }
-  };
 
-  const handleEditProduct = (id: string) => {
-    // Navegar a la página de editProduct y pasar el ID del producto como un parámetro en la URL
-    localStorage.setItem('productID', id);
-    router.push("/pages/admin/editProduct");
-  }
+    const handleViewDetails = (id: string) => {
+      // Navegar a la página de productDetails y pasar el ID del producto como un parámetro en la URL
+      localStorage.setItem('productID', id);
+      router.push("/pages/users/productDetails");
+    };
 
-  const handleViewDetails = (id: string) => {
-    // Navegar a la página de productDetails y pasar el ID del producto como un parámetro en la URL
-    localStorage.setItem('productID', id);
-    router.push("/pages/users/productDetails");
-  };
+    return (
+      <div>
+        <AdminNavbar />
+        <div className="bg-pink-lighter min-h-screen p-6">
+          <h1>
+            <title>Tienda</title>
+          </h1>
+          <div className="flex">
+            {/* Sección de Categorías y Subcategorías */}
+            <div
+              className="w-1/4 bg-white border rounded-md p-4 overflow-y-auto"
+              style={{ maxHeight: "80vh" }}
+            >
+              <div className="flex items-center justify-between mb-4">
+                <div className="flex">
+                  <input
+                    type="text"
+                    placeholder="Buscar en la tienda..."
+                    className="border rounded-md p-1.5 text-sm w-2/3"
+                    value={searchQuery}
+                    onChange={(e) => setSearchQuery(e.target.value)}
+                  />
+                  <button className="boton-global ml-2" onClick={handleSearch}>Buscar</button>
 
-  return (
-    <div>
-      <AdminNavbar />
-      <div className="bg-pink-lighter min-h-screen p-6">
-        <h1>
-          <title>Tienda</title>
-        </h1>
-        <div className="flex">
-          {/* Sección de Categorías y Subcategorías */}
-          <div
-            className="w-1/4 bg-white border rounded-md p-4 overflow-y-auto"
-            style={{ maxHeight: "80vh" }}
-          >
-            <div className="flex items-center justify-between mb-4">
-              <div className="flex">
-                <input
-                  type="text"
-                  placeholder="Buscar en la tienda..."
-                  className="border rounded-md p-1.5 text-sm w-2/3"
-                />
-                <button className="boton-global ml-2">Buscar</button>
-
-                {/* Botones replicados */}
-                <div className="ml-2 relative">
-                  <button
-                    onClick={() => toggleDropdown(-1)} // Aquí usamos -1 para identificar este menú desplegable específico
-                    style={{ fontSize: "10px", lineHeight: "10px" }}
-                  >
-                    •<br />•<br />•
-                  </button>
-                  {dropdownVisible === -1 && ( // Aquí mostramos el menú si dropdownVisible es -1
-                    <div className="absolute mt-2 right-0 w-24 bg-white border rounded-md overflow-hidden">
-                      <Link href={"/pages/admin/addCategory"}>
-                        <button className="block w-full text-left px-2 py-1 text-sm">
-                          Agregar categoría
-                        </button>
-                      </Link>
-                      <Link href={"/pages/admin/addSubcategory"}>
-                        <button className="block w-full text-left px-2 py-1 text-sm">
-                          Agregar subcategoría
-                        </button>
-                      </Link>
-
-                      <button className="block w-full text-left px-2 py-1 text-sm">
-                        Eliminar
-                      </button>
-                    </div>
-                  )}
-                </div>
-                {/* Fin de botones replicados */}
-              </div>
-            </div>
-            <h2 className="text-xl font-bold mb-4">Categorías</h2>
-            <ul className="mb-8 space-y-2">
-              {categories.map((category) => (
-                <li key={category.name}>
-                  <div className="flex items-center">
-                    <input type="checkbox" />
-                    <span className="ml-2">{category.name}</span>
-                  </div>
-                  {category.subcategories.length > 0 && (
-                    <ul className="ml-6 space-y-2 mt-2">
-                      {category.subcategories.map((subcategory) => (
-                        <li className="flex items-center" key={subcategory}>
-                          <input type="checkbox" />
-                          <span className="ml-2">{subcategory}</span>
-                        </li>
-                      ))}
-                    </ul>
-                  )}
-                </li>
-              ))}
-            </ul>
-          </div>
-          <div
-            className="flex-1 bg-white border rounded-md p-4 overflow-y-auto ml-6"
-            style={{ maxHeight: "80vh" }}
-          >
-            <div className="my-4">
-              <Link
-                href="/pages/admin/addProduct"
-                className="boton-global mb-4"
-              >
-                <button>+ Agregar</button>
-              </Link>
-              <Link
-                href="/pages/admin/orders"
-                className="boton-global mb-4 ml-4"
-              >
-                <button>Pedidos</button>
-              </Link>
-            </div>
-
-            <div className="grid grid-cols-3 gap-4">
-              {gallery.map((image, idx) => (
-                <div key={idx} className="border rounded-md p-4 relative">
-                  {/* Menú desplegable */}
-                  <div className="absolute top-0 right-0 mt-2 mr-2">
+                  {/* Botones replicados */}
+                  <div className="ml-2 relative">
                     <button
-                      onClick={() => toggleDropdown(idx)}
+                      onClick={() => toggleDropdown(-1)} // Aquí usamos -1 para identificar este menú desplegable específico
                       style={{ fontSize: "10px", lineHeight: "10px" }}
                     >
                       •<br />•<br />•
                     </button>
-                    {dropdownVisible === idx && (
+                    {dropdownVisible === -1 && ( // Aquí mostramos el menú si dropdownVisible es -1
                       <div className="absolute mt-2 right-0 w-24 bg-white border rounded-md overflow-hidden">
-                        {/* <Link
-                          className="block w-full text-left px-2 py-1 text-sm"
-                          href="/pages/admin/editProduct"
-                        > */}
-                        <button onClick={() => handleEditProduct(image.id)}>Editar</button>
-                        {/* </Link> */}
-                        <button className="block w-full text-left px-2 py-1 text-sm" onClick={() => handleDeleteProduct(image.id)}>
+                        <Link href={"/pages/admin/addCategory"}>
+                          <button className="block w-full text-left px-2 py-1 text-sm">
+                            Agregar categoría
+                          </button>
+                        </Link>
+                        <Link href={"/pages/admin/addSubcategory"}>
+                          <button className="block w-full text-left px-2 py-1 text-sm">
+                            Agregar subcategoría
+                          </button>
+                        </Link>
+
+                        <button className="block w-full text-left px-2 py-1 text-sm">
                           Eliminar
                         </button>
                       </div>
                     )}
                   </div>
+                  {/* Fin de botones replicados */}
+                </div>
+              </div>
+              <h2 className="text-xl font-bold mb-4">Categorías</h2>
+              <ul className="mb-8 space-y-2">
+                {categories.map((category) => (
+                  <li key={category.name}>
+                    <div className="flex items-center">
+                      <input type="checkbox" />
+                      <span className="ml-2">{category.name}</span>
+                    </div>
+                    {category.subcategories.length > 0 && (
+                      <ul className="ml-6 space-y-2 mt-2">
+                        {category.subcategories.map((subcategory) => (
+                          <li className="flex items-center" key={subcategory}>
+                            <input type="checkbox" />
+                            <span className="ml-2">{subcategory}</span>
+                          </li>
+                        ))}
+                      </ul>
+                    )}
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div
+              className="flex-1 bg-white border rounded-md p-4 overflow-y-auto ml-6"
+              style={{ maxHeight: "80vh" }}
+            >
+              <div className="my-4">
+                <Link
+                  href="/pages/admin/addProduct"
+                  className="boton-global mb-4"
+                >
+                  <button>+ Agregar</button>
+                </Link>
+                <Link
+                  href="/pages/admin/orders"
+                  className="boton-global mb-4 ml-4"
+                >
+                  <button>Pedidos</button>
+                </Link>
+              </div>
 
-                  <div className="mb-4">
-                    <img
-                      src={image.imgSrc}
-                      className="w-full h-32 object-cover rounded-md"
-                    />
-                  </div>
-                  <div className="flex items-center justify-between">
-                    <div className="text-gray-700">{image.title}</div>
-                    {/* <Link
+              <div className="grid grid-cols-3 gap-4">
+                {renderItems.map((image, idx) => (
+                  <div key={idx} className="border rounded-md p-4 relative">
+                    {/* Menú desplegable */}
+                    <div className="absolute top-0 right-0 mt-2 mr-2">
+                      <button
+                        onClick={() => toggleDropdown(idx)}
+                        style={{ fontSize: "10px", lineHeight: "10px" }}
+                      >
+                        •<br />•<br />•
+                      </button>
+                      {dropdownVisible === idx && (
+                        <div className="absolute mt-2 right-0 w-24 bg-white border rounded-md overflow-hidden">
+                          {/* <Link
+                          className="block w-full text-left px-2 py-1 text-sm"
+                          href="/pages/admin/editProduct"
+                        > */}
+                          <button onClick={() => handleEditProduct(image.id)}>Editar</button>
+                          {/* </Link> */}
+                          <button className="block w-full text-left px-2 py-1 text-sm" onClick={() => handleDeleteProduct(image.id)}>
+                            Eliminar
+                          </button>
+                        </div>
+                      )}
+                    </div>
+
+                    <div className="mb-4">
+                      <img
+                        src={image.imgSrc}
+                        className="w-full h-32 object-cover rounded-md"
+                      />
+                    </div>
+                    <div className="flex items-center justify-between">
+                      <div className="text-gray-700">{image.title}</div>
+                      {/* <Link
                         href="/pages/users/imageDetails"
                         className="text-blue-500"
                       > */}
-                    <button onClick={() => handleViewDetails(image.id)}>Ver más</button>
-                    {/* </Link> */}
+                      <button onClick={() => handleViewDetails(image.id)}>Ver más</button>
+                      {/* </Link> */}
+                    </div>
+                    <div className="mt-2 text-center text-gray-800">
+                      {image.price}
+                    </div>
                   </div>
-                  <div className="mt-2 text-center text-gray-800">
-                    {image.price}
-                  </div>
-                </div>
-              ))}
+                ))}
+              </div>
             </div>
           </div>
         </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
-  );
-};
+    );
+  };
 
-export default Shop;
+  export default Shop;
