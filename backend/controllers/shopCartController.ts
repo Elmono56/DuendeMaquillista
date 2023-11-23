1ffbb449e3a52de080d73651ff9846b1dc3568f0
Kevin <78829208+Kev1606@users.noreply.github.com>
2023-11-23 15:21:55 -0600

    Fix: change method on controller (axios)

--- backend/controllers/shopCartController.ts
+++ backend/controllers/shopCartController.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 class ShopCartController{
     async updateShopCart(route: string, data: any){
-        const response = await axios.put(route, data);
+        const response = await axios.post(route, data);
         return response;
     }
     async getShopCart(route: string, id: any){
